4468b32df4126db4441f4f98f104e14bd17350a0
Jonathan May <jonathan.may@devmountain.com>
2019-01-10 09:43:43 -0600

    Create basic server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const { json } = require("body-parser");
+const massive = require("massive");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const app = express();
+
+app.listen(process.env.PORT || 5050, () => {
+  console.log(`Listening on ${process.env.PORT}`);
+});
